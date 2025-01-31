e6363434f20d9de99194562f6971fc8a7c8c5bdf
EclipseIsDead <sidmcheath@gmail.com>
2025-01-31 11:18:39 -0500

    Basic Window setup

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# DungeonPlanner
+# Dungeon Planner
+
+A minimal D&D dungeon mapper in C++.
+
+## Dependencies
+- OpenGL 3.3
+- GLFW3
+- GLAD
+
+## Build
+
+Change the main shader paths in main.cpp and use absolute pathing. To build yourself, run:
+
+```bash
+cmake .
+make
+./DungeonPlanner
+```
+
+Note:
+- Read CMakeLists.txt for more details on build.
+- Assumes brew install of GLFW3 and GLM.
+
+If you want, you can delete dependencies and move the include and lib contents to usr/local/include and usr/local/lib respectively. Just adjust the CMakeLists.txt file accordingly.
---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+/* Ask for an OpenGL Core Context */
+#define GLFW_INCLUDE_NONE  // Macro Redefinition
+#define GL_SILENCE_DEPRECATION  // THERE IS A REASON I AM USING 3.3
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+#include "shader.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Dungeon Planner", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Create and compile shaders
+    Shader particleShader("/Users/sid/Documents/Workspace/DungeonPlanner/src/shaders/vertex.glsl", "/Users/sid/Documents/Workspace/DungeonPlanner/src/shaders/fragment.glsl");
+
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Input
+        processInput(window);
+
+        // Render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set the clear color
+        glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+
+        // Swap buffers and poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+// CALLBACKS
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
